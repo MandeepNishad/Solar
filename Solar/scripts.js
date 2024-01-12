2661006214495325802a20587d8b0be958384f1d
MandeepNishad <76830315+MandeepNishad@users.noreply.github.com>
2024-01-12 17:14:45 +0530

    Add files via upload

---
+++ Solar/scripts.js
@@ -0,0 +1,33 @@
+const allStar = document.querySelectorAll('.rating .star')
+const ratingValue = document.querySelector('.rating input')
+
+allStar.forEach((item, idx)=> {
+	item.addEventListener('click', function () {
+		let click = 0
+		ratingValue.value = idx + 1
+
+		allStar.forEach(i=> {
+			i.classList.replace('bxs-star', 'bx-star')
+			i.classList.remove('active')
+		})
+		for(let i=0; i<allStar.length; i++) {
+			if(i <= idx) {
+				allStar[i].classList.replace('bx-star', 'bxs-star')
+				allStar[i].classList.add('active')
+			} else {
+				allStar[i].style.setProperty('--i', click)
+				click++
+			}
+		}
+	})
+})
+
+function subscribe() {
+    // Get the email input value (you can perform validation here)
+    var emailInput = document.getElementsByTagName('button').value;
+
+    // Display thank you message and hide the subscription form
+
+    var thankYouMessage = document.getElementById('thank-you-message');
+    thankYouMessage.style.display = 'block';
+}
